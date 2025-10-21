aa1e0acb579915dba413fae79e772cac58d4547e
Manoah Bernier <manoah.bernier@pm.me>
2025-10-21 02:56:54 +0200

    Remove IDE files and test project from version control
    - Remove .idea/ directory (PyCharm IDE files)
    - Remove testlib/ directory (test project created during development)
    - These should not be tracked in the repository

--- testlib/src/testlib/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Package testlib."""
-
-__version__ = "0.1.0"
--- testlib/tests/test_sanity.py
+++
@@ -1,8 +0,0 @@
-"""Basic tests for testlib."""
-
-import testlib
-
-
-def test_version():
-    """Test package version."""
-    assert testlib.__version__ == "0.1.0"
